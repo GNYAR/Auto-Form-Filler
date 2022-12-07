e50c3105d126f75d93d4d3f52310f3558da822ad
GNYAR <s901121s901121@gmail.com>
2022-12-07 19:30:55 +0800

    feat(menu): implement the function menu

--- ais/ais.py
+++ ais/ais.py
@@ -13,6 +13,47 @@ pre = {
 options.add_experimental_option("prefs", pre)
 driver = webdriver.Edge(options = options)
 
+# functions
+def courseEvaluationSurvey():
+  goToNode(["Menu_TreeViewt1", "Menu_TreeViewt28", "Menu_TreeViewt39"])
+  # loop
+  while(True):
+    try:
+      # select a form
+      driver.switch_to.frame("mainFrame")
+      driver.find_element(By.XPATH, "//td[@onclick]//a[@href='#this']").click()
+      driver.switch_to.default_content()
+
+      sleep(3) # wait for page change
+
+      # auto filler
+      driver.switch_to.frame("viewFrame")
+      radio = driver.find_elements(By.XPATH, "//input[@type='radio']")
+      for i in range(len(radio) // 5): # 5 options for each question
+        radio[i * 5 + 2].click() # select the middle one
+      driver.find_element(By.NAME, "SAVE_BTN2").click()
+      sleep(1) # wait for alert
+      driver.switch_to.alert.accept()
+      sleep(1) # wait for alert
+      driver.switch_to.alert.accept()
+      driver.switch_to.default_content()
+    except:
+      break
+
+def funcMenu(funcs):
+  print("Function Menu")
+  for i in funcs:
+    print(str(i["id"]) + ".  " + i["text"])
+  print("\n0.  Exit")
+  return int(input("Enter the number: "))
+
+def goToNode(path):
+  driver.switch_to.frame("menuFrame")
+  for i in path:
+    driver.find_element(By.ID, i).click()
+    sleep(1) # wait for animation
+  driver.switch_to.default_content()
+
 # variables
 url = "https://ais.ntou.edu.tw"
 
@@ -41,46 +82,24 @@ while(True):
     break
 print("Login successfully.\n")
 
-# go to menu
-driver.switch_to.frame("menuFrame")
-driver.find_element(By.ID, "Menu_TreeViewt1").click() # 教務系統
-sleep(1) # wait for animation
-driver.find_element(By.ID, "Menu_TreeViewt28").click() # 教學評鑑
-sleep(1) # wait for animation
-driver.find_element(By.ID, "Menu_TreeViewt39").click() # 填寫問卷
-sleep(1) # wait for animation
-driver.switch_to.default_content()
-
-# loop
-isError = False
-while(True):
-  try:
-    # select a form
-    driver.switch_to.frame("mainFrame")
-    driver.find_element(By.XPATH, "//td[@onclick]//a[@href='#this']").click()
-    driver.switch_to.default_content()
-
-    sleep(3) # wait for page change
-
-    # auto filler
-    driver.switch_to.frame("viewFrame")
-    radio = driver.find_elements(By.XPATH, "//input[@type='radio']")
-    for i in range(len(radio) // 5): # 5 options for each question
-      radio[i * 5 + 2].click() # select the middle one
-    driver.find_element(By.NAME, "SAVE_BTN2").click()
-    sleep(1) # wait for alert
-    driver.switch_to.alert.accept()
-    sleep(1) # wait for alert
-    driver.switch_to.alert.accept()
-    driver.switch_to.default_content()
-  except:
-    isError = True
+# function menu
+funcs = [
+  {
+    "id": 1,
+    "text": "Course Evaluation Survey",
+    "func": courseEvaluationSurvey
+  }
+]
+selected = funcMenu(funcs)
+while(selected != 0):
+  filtered = [x for x in funcs if x["id"] == selected]
+  if len(filtered):
+    print("\nStart the function.")
+    filtered[0]["func"]()
+    print("Done! Please check.")
     break
-
-# end
-if(isError):
-  print("Error Occurred.")
-else:
-  print("Done! Please check.")
+  else:
+    print("Bad input.\n")
+    funcMenu(funcs)
 
 system("pause")
